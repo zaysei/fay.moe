42d52f3c007fdfb1779c022e90e571771301a49f
faye <hendersonz2023@gmail.com>
2024-07-28 00:00:45 -0500

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zaysei.github.io',
-    base: '/fay.moe'
+    site: 'https://fay.moe',
 });
 
 
