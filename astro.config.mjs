fe73437532479c9d3b1e59e1665cfa5f9c90dc10
faye <hendersonz2023@gmail.com>
2024-07-31 00:07:37 -0500

    Finished! Will archive this or I will improve upon it soon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://fay.moe',
-})
-
-
+  site: 'https://fay.moe',
+  integrations: [preact()]
+});
